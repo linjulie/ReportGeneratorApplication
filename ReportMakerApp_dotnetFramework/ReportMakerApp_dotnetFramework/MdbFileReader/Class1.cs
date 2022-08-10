9fb33baa6c3b7df5cc19cb1e23a696e547b1fa72
Lin Julie Fjeldvik <99178418+linjulie@users.noreply.github.com>
2022-08-10 13:15:26 +0200

    Add files via upload
    ReportMakerApp_dotnetFramework

---
+++ ReportMakerApp_dotnetFramework/ReportMakerApp_dotnetFramework/MdbFileReader/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportMakerApp_dotnetFramework.MdbFileReader
+{
+    internal class Class1
+    {
+        public static void ReadData(string connectionstring, string querystring)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connectionstring))
+            {
+                connection.ConnectionString = connectionstring;
+                connection.Open();
+                using (OleDbCommand command = new OleDbCommand(querystring, connection))
+                {
+                    using (OleDbDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader.GetString(0));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
